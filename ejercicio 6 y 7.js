8ff25a21ba23d91ae3a4352907cacb75dbc063bb
donatocasanova <donatocasanovaa11@gmail.com>
2026-01-16 15:11:42 -0300

    Add files via upload

---
+++ ejercicio 6 y 7.js
@@ -0,0 +1,26 @@
+const form = document.getElementById("formulario");
+
+const h1 = document.createElement("h1");
+h1.id = "resultado"; 
+document.body.appendChild(h1);
+
+
+
+function calcular(event){
+    event.preventDefault();
+    const a = form.a.value;
+    const b = form.b.value;
+
+    const res = a**b;
+    
+    h1.textContent = res;
+
+}
+
+
+
+form.addEventListener("submit" ,calcular)
+
+
+
+
